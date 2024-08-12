6769785c41748f85aab13953865e89f71fa7b13d
rohanxxx <mrabbi@fordham.edu>
2024-08-12 03:16:58 -0400

    created the architecture of Lunar lendings neural network

--- lunar_landing.py
+++ lunar_landing.py
@@ -7,3 +7,16 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torch.autograd as autograd
 from collections import deque, namedtuple
+
+#part1- building the AI
+
+#network class
+#architecture of our neural network
+#the brain of AI
+class Network(nn.Module):
+    def __init__(self, state_size, action_size, seed = 42) -> None: #means nothing will be returened 
+        super(Network, self).__init__() #activates the inheritance
+        self.seed = torch.manual_seed(seed) #generates the randome vector
+        self.fc1 = nn.Linear(state_size, 64) #representing the first input layer between the full connected layer
+        self.fc2 = nn.Linear(64, 64) #2nd fully connected layer between 1st and full connected layer
+        self.fc3 = nn.Linear(64, action_size) 
